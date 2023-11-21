b743ae21ded6c8998dc9274bca0d6a0e6ca43ea1
Prisca C <prisca.castanier@gmail.com>
2023-11-21 14:06:18 +0100

    refactor: move routes and style app div

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import Routes from './components/Routes.tsx';
+import styled from 'styled-components';
+
 function App() {
-  return <></>;
+  const Div = styled.div`
+    height: 100vh;
+    width: 100vw;
+  `;
+
+  return (
+    <Div>
+      <Routes />
+    </Div>
+  );
 }
 
 export default App;
