f94524a269a41bec84f857e6cb0efeed95d2399b
Prisca <prisca.castanier@gmail.com>
2023-11-21 20:01:10 +0100

    refactor(navbar): move logo from navbar into a single file

---
+++ src/components/Navbar/LogoNavItem.tsx
@@ -0,0 +1,25 @@
+import { Logo } from '../Logo.tsx';
+import styled from 'styled-components';
+import { theme } from '../../theme';
+
+export const LogoNavItem = () => {
+  return (
+    <LogoText>
+      Cake
+      <Logo width={'clamp(2rem, 45px, 3rem)'} />
+      Nest
+    </LogoText>
+  );
+};
+
+const LogoText = styled.p`
+  font-family: 'Open Sans', sans-serif;
+  font-size: clamp(1.5rem, ${theme.fonts.size.P3}, 2rem);
+  font-weight: bold;
+  text-transform: uppercase;
+  color: ${theme.colors.primary};
+  gap: 0.2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,16 +1,11 @@
 import styled from 'styled-components';
-import { Logo } from '../Logo.tsx';
-import { theme } from '../../theme';
 import { UserNavItem } from './UserNavItem.tsx';
+import { LogoNavItem } from './LogoNavItem.tsx';
 
 export const Navbar = () => {
   return (
     <Div>
-      <LogoText>
-        Cake
-        <Logo width={'20px'} />
-        Nest
-      </LogoText>
+      <LogoNavItem />
       <UserNavItem />
     </Div>
   );
@@ -20,21 +15,9 @@ const Div = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  height: 10vh;
+  height: clamp(3rem, 10vw, 5rem);
   background-color: #fff;
   color: #000;
   border-radius: 10px 10px 0 0;
   padding: 0 20px;
-`;
-
-const LogoText = styled.p`
-  font-family: 'Open Sans', sans-serif;
-  font-size: 1rem;
-  font-weight: bold;
-  text-transform: uppercase;
-  color: ${theme.colors.primary};
-  gap: 0.2rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
 `;
