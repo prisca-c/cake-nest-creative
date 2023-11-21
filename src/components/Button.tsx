1537db8b1ac68c1bafa61b26c5ed6cdb5addb6f2
Prisca C <prisca.castanier@gmail.com>
2023-11-21 15:37:38 +0100

    refactor(button): move button style outside and use styled-component props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,23 +5,47 @@ import { theme } from '../theme';
 type ButtonProps = {
   variant: keyof typeof theme.colors;
   children: React.ReactNode;
+  width: string;
   onClick?: () => void;
+  textAlign?: string;
 };
 
-export const Button = ({ variant, children, onClick }: ButtonProps) => {
-  const Button = styled.button`
-    background-color: ${theme.colors[variant]};
-    color: ${theme.colors.white};
-    border: none;
-    border-radius: 5px;
-    padding: 10px 15px;
-    font-size: 1.2rem;
-    cursor: pointer;
-    transition: background-color 0.3s ease-in-out;
-    display: flex;
-    place-items: center;
-    gap: 10px;
-  `;
-
-  return <Button onClick={onClick}>{children}</Button>;
+export const Button = ({
+  variant,
+  children,
+  onClick,
+  width,
+  textAlign = 'center',
+}: ButtonProps) => {
+  return (
+    <ButtonStyle
+      $width={width}
+      onClick={onClick}
+      $textAlign={textAlign}
+      $variant={variant}
+    >
+      {children}
+    </ButtonStyle>
+  );
 };
+
+const ButtonStyle = styled.button<{
+  $variant: keyof typeof theme.colors;
+  $width: string;
+  $textAlign: string;
+}>`
+  background-color: ${({ $variant }) => theme.colors[$variant]};
+  color: ${theme.colors.white};
+  border: none;
+  border-radius: 5px;
+  padding: 10px 15px;
+  font-size: 1.2rem;
+  cursor: pointer;
+  transition: background-color 0.3s ease-in-out;
+  gap: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: ${({ $width }) => $width};
+  text-align: ${({ $textAlign }) => $textAlign};
+`;
