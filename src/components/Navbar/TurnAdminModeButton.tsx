4758ea35f6a65702998208e150664af8e558d9ad
Prisca C <prisca.castanier@gmail.com>
2023-11-22 14:19:58 +0100

    refactor(style): changes admin switch button style

--- src/components/Navbar/TurnAdminModeButton.tsx
+++ src/components/Navbar/TurnAdminModeButton.tsx
@@ -11,18 +11,25 @@ export const TurnAdminModeButton = ({
   onClick,
 }: TurnAdminModeButtonProps) => {
   const text = active ? 'Désactiver' : 'Activer';
+  const handleClassActive = () => {
+    return active ? 'active' : '';
+  };
+
   return (
-    <AdminButton $active={active} onClick={onClick}>
-      <Circle $active={active} />
+    <AdminButton
+      $active={active}
+      onClick={onClick}
+      className={handleClassActive()}
+    >
+      <Circle $active={active} className={handleClassActive()} />
       <p>{text} le mode admin</p>
     </AdminButton>
   );
 };
 
 const AdminButton = styled.button<{ $active: boolean }>`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+  position: relative;
+  display: block;
   gap: 5px;
   background-color: ${theme.colors.incognito};
   border-radius: 100px;
@@ -33,25 +40,39 @@ const AdminButton = styled.button<{ $active: boolean }>`
   border: none;
   transition: all 0.2s ease-in-out;
   width: 200px;
+  height: 34px;
 
   p {
+    position: absolute;
     font-size: 0.6rem;
     font-weight: 600;
     padding: 0 15px 0 0;
+    transform: translateY(-50%);
+    top: 50%;
+    right: 0;
+    transition: all 0.3s ease-in-out;
+  }
+
+  &.active p {
+    right: 15px;
+    left: 0;
   }
 `;
 
 const Circle = styled.div<{ $active: boolean }>`
+  position: absolute;
   width: 25px;
   height: 25px;
   border-radius: 50%;
   background-color: ${({ $active }) =>
     $active ? theme.colors.primary : theme.colors.red};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 0.8rem;
-  font-weight: 600;
   border: 4px solid ${theme.colors.incognito};
-  transition: all 0.2s ease-in-out;
+  transition: all 0.3s ease-in-out;
+  left: 0;
+  top: 50%;
+  transform: translateY(-50%);
+
+  &.active {
+    left: calc(100% - 34px);
+  }
 `;
