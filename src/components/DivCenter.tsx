e719d08dadf98a199988e438fbc3fc480a43d2c5
Prisca <prisca.castanier@gmail.com>
2023-11-23 16:19:21 +0100

    refactor(order): add context and select menu components to order menu

--- src/components/DivCenter.tsx
+++ src/components/DivCenter.tsx
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
-export const DivCenter = styled.div`
+export const DivCenter = styled.div<{ $height?: string; $width?: string }>`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 100%;
-  height: 100%;
+  width: ${({ $width }) => $width || '100%'};
+  height: ${({ $height }) => $height || 'auto'};
 `;
