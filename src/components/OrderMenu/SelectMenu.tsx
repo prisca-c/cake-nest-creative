b1fa0fbc08dde4b12761189eddce1229e8fb83ad
Prisca <prisca.castanier@gmail.com>
2023-11-23 16:18:46 +0100

    feat(menus): add select menu component to handle menu selection

---
+++ src/components/OrderMenu/SelectMenu.tsx
@@ -0,0 +1,31 @@
+import { useContext } from 'react';
+import { MenusContext } from '../../context/MenusContext.ts';
+import styled from 'styled-components';
+import { Button } from '../Button.tsx';
+
+export const SelectMenu = () => {
+  const { menus, selectedMenu, setSelectedMenu } = useContext(MenusContext);
+  return (
+    <Main>
+      {menus.map((menu) => (
+        <Button
+          key={menu.id}
+          onClick={() => setSelectedMenu(menu.id)}
+          active={menu.id === selectedMenu}
+          variant={'primary'}
+          width={'200px'}
+        >
+          {menu.name}
+        </Button>
+      ))}
+    </Main>
+  );
+};
+
+const Main = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 10px;
+  width: fit-content;
+`;
