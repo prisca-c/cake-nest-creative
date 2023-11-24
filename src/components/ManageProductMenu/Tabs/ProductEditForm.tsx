07b83997e6d0a81bb804149cd479d6ad8d48ecd6
Prisca C <prisca.castanier@gmail.com>
2023-11-24 16:50:26 +0100

    feat(admin): now possible to edit in real time product

---
+++ src/components/ManageProductMenu/Tabs/ProductEditForm.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { FaCamera } from 'react-icons/fa';
+import { GiCupcake } from 'react-icons/gi';
+import { MdEuro } from 'react-icons/md';
+import styled from 'styled-components';
+import type { ManageProductType } from '@Types/ManageProductType.ts';
+
+import { theme } from '~@/theme';
+import { useEditProductForm } from '@Hooks/useEditProductForm.ts';
+
+type ProductAddFormProps = {
+  data: ManageProductType;
+  setData: React.Dispatch<React.SetStateAction<ManageProductType>>;
+};
+
+export const ProductEditForm = ({ data, setData }: ProductAddFormProps) => {
+  const { handleChange } = useEditProductForm({ data, setData });
+
+  return (
+    <Main>
+      <div className={'input-group'}>
+        <GiCupcake color={theme.colors.greyDark} size={20} />
+        <input
+          type="text"
+          placeholder="Nom du produit"
+          onChange={handleChange}
+          value={data.name}
+          id="name"
+        />
+      </div>
+      <div className={'input-group'}>
+        <FaCamera color={theme.colors.greyDark} size={20} />
+        <input
+          type="text"
+          placeholder="Lien URL de l'image (ex: https://mon-url.me/mon-produit.png)"
+          onChange={handleChange}
+          value={data.image}
+          id="image"
+        />
+      </div>
+      <div className={'input-group'}>
+        <MdEuro color={theme.colors.greyDark} size={20} />
+        <input
+          type="number"
+          placeholder="Prix du produit"
+          onChange={handleChange}
+          value={data.price}
+          step="0.01"
+          id="price"
+        />
+      </div>
+      <p>Cliquez sur un produit pour le modifier en temps réel</p>
+    </Main>
+  );
+};
+
+const Main = styled.div`
+  .input-group {
+    display: flex;
+    align-items: center;
+    border-radius: ${theme.borderRadius.round};
+    background-color: ${theme.colors.greyLight};
+    padding: 10px 20px;
+    margin-bottom: 10px;
+
+    input {
+      border: none;
+      outline: none;
+      font-size: 16px;
+      margin-left: 10px;
+      background-color: ${theme.colors.greyLight};
+      width: 100%;
+    }
+  }
+
+  p {
+    color: ${theme.colors.success};
+    font-size: 14px;
+  }
+`;
---
+++ src/hooks/useEditProductForm.ts
@@ -0,0 +1,75 @@
+import React, { useContext, useEffect } from 'react';
+import type { ManageProductType } from '@Types/ManageProductType.ts';
+import { MenusContext } from '@Context/MenusContext.ts';
+import { AdminModeContext } from '@Context/AdminModeContext.ts';
+
+type UseEditProductFormProps = {
+  data: ManageProductType;
+  setData: React.Dispatch<React.SetStateAction<ManageProductType>>;
+};
+
+export const useEditProductForm = ({
+  data,
+  setData,
+}: UseEditProductFormProps) => {
+  const { selectedMenu, menus, setMenus } = useContext(MenusContext);
+  const { selectedProduct } = useContext(AdminModeContext);
+
+  const { productId, menuId } = selectedProduct;
+
+  useEffect(() => {
+    handleData();
+  }, []);
+
+  useEffect(() => {
+    handleUpdateProduct();
+  }, [data]);
+
+  const handleData = () => {
+    const menu = menus.find((menu) => menu.id === selectedProduct.menuId);
+    const product = menu?.products.find(
+      (product) => product.id === selectedProduct.productId,
+    );
+    if (product) {
+      setData({
+        name: product.title,
+        image: product.imageSource,
+        price: product.price,
+      });
+    }
+  };
+
+  const handleUpdateProduct = () => {
+    const newMenus = menus.map((menu) => {
+      if (menu.id === menuId) {
+        return {
+          ...menu,
+          products: menu.products.map((product) => {
+            if (product.id === productId) {
+              return {
+                ...product,
+                title: data.name,
+                imageSource: data.image,
+                price: data.price,
+              };
+            }
+            return product;
+          }),
+        };
+      }
+      return menu;
+    });
+    setMenus(newMenus);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setData({
+      ...data,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  return {
+    handleChange,
+  };
+};
