9763347397c4c81e06ff29ff9f3b6cb76ae8df0c
Prisca C <prisca.castanier@gmail.com>
2023-12-01 12:26:28 +0100

    feat(discount): add message to handle submit response to display

--- src/ui/components/Cart/AddDiscount.tsx
+++ src/ui/components/Cart/AddDiscount.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { DiscountsContext } from '@Context/DiscountsContext.ts';
 import styled from 'styled-components';
 import { theme } from '~@/ui/theme';
@@ -7,10 +7,19 @@ import { BiRightArrowCircle } from 'react-icons/bi';
 import { Button } from '~@/ui/components/Button.tsx';
 import { useUpdateCartUseCases } from '~@/usecases/useUpdateCartUseCases.ts';
 import { isValidDiscount } from '@Utils/discountHelper.ts';
+import { useTimer } from '@Hooks/useTimer.ts';
 
 export const AddDiscount = () => {
   const { discounts } = useContext(DiscountsContext);
   const { cart } = useContext(CartContext);
+  const [submitMessage, setSubmitMessage] = useState<{
+    text: string;
+    color: string;
+  }>({
+    text: '',
+    color: '',
+  });
+  const { timerState, setTimerState } = useTimer({ time: 3000 });
   const { updateCart } = useUpdateCartUseCases();
   const handleAddDiscount = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -25,60 +34,102 @@ export const AddDiscount = () => {
     );
 
     discounts.forEach((discount) => {
-      if (!isValidDiscount(discount)) return;
       if (
         discount.code.toLowerCase() === code.toLowerCase() &&
         !discountExists &&
         (newCart.discounts.length === 0 || discount.cumulative) &&
         !newCartHasNonCumulativeDiscount
       ) {
+        if (!isValidDiscount(discount)) {
+          setSubmitMessage({
+            text: "Le code de réduction n'est plus valide",
+            color: theme.colors.red,
+          });
+          setTimerState(true);
+          return;
+        }
         newCart.discounts = [...newCart.discounts, discount];
-        updateCart(newCart).catch(() => console.error('Something went wrong'));
+        updateCart(newCart).catch(() =>
+          console.error(() =>
+            setSubmitMessage({
+              text: 'Une erreur est survenue',
+              color: theme.colors.red,
+            }),
+          ),
+        );
       }
     });
+
+    if (discountExists) {
+      setSubmitMessage({
+        text: 'Le code de réduction a déjà été ajouté',
+        color: theme.colors.red,
+      });
+      setTimerState(true);
+      return;
+    }
+
+    e.currentTarget.code.value = '';
   };
 
   return (
     <Main>
-      <p>Code de réduction</p>
-      <form onSubmit={handleAddDiscount}>
-        <input type="text" placeholder="Code" id={'code'} />
-        <Button variant={'white'} width={'fit-content'} padded={false}>
-          <BiRightArrowCircle
-            size={20}
-            color={theme.colors.background_dark}
-            className={'add-icon'}
-          />
-        </Button>
-      </form>
+      <div className={'discount_content'}>
+        <p>Code de réduction</p>
+        <form onSubmit={handleAddDiscount}>
+          <input type="text" placeholder="Code" id={'code'} />
+          <Button variant={'white'} width={'fit-content'} padded={false}>
+            <BiRightArrowCircle
+              size={20}
+              color={theme.colors.background_dark}
+              className={'add-icon'}
+            />
+          </Button>
+        </form>
+      </div>
+      {timerState && (
+        <p className={'submit_message'} style={{ color: submitMessage.color }}>
+          {submitMessage.text}
+        </p>
+      )}
     </Main>
   );
 };
 
 const Main = styled.div`
   display: flex;
-  gap: 10px;
-  align-items: center;
-  background-color: ${theme.colors.primary};
-  padding: 15px 10px;
-  height: 20px;
-  width: calc(100% - 20px - 4px);
-  color: ${theme.colors.background_dark};
-  border: 2px solid ${theme.colors.background_dark};
-
-  form {
+  flex-direction: column;
+  .discount_content {
     display: flex;
     gap: 10px;
     align-items: center;
+    background-color: ${theme.colors.primary};
+    padding: 15px 10px;
+    height: 20px;
+    width: calc(100% - 20px - 4px);
+    color: ${theme.colors.background_dark};
+    border: 2px solid ${theme.colors.background_dark};
 
-    input {
-      border: none;
-      border-radius: 5px;
-      padding: 5px;
-    }
+    form {
+      display: flex;
+      gap: 10px;
+      align-items: center;
+      flex: 1;
 
-    .add-icon {
-      cursor: pointer;
+      input {
+        border: none;
+        border-radius: 5px;
+        padding: 5px;
+        width: 100%;
+      }
+
+      .add-icon {
+        cursor: pointer;
+      }
     }
+  }
+
+  .submit_message {
+    text-align: center;
   }
 `;
